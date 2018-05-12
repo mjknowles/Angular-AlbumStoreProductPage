f0b9321b82a769f0000cfd364776fafa478574ba
mjknowles <mjknowles23@gmail.com>
2018-05-12 16:20:53 -0500

    Adding track listing component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album'
 import { Observable } from 'rxjs/Observable'
+
 @Injectable()
 export class ProductService {
   private _albumUrl: string = "../assets/album.json"
